43824f8bb97d75bf472a427dddc29432f77ef849
Jonathan Shoemaker <syco1316@gmail.com>
2022-10-25 11:31:07 -0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Jonathan Shoemaker on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
